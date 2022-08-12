5e9c200b10b5165da8c96d640b05d072355d064a
sborms <samuel.borms@unine.ch>
2022-08-12 10:43:55 +0200

    init

---
+++ app.py
@@ -0,0 +1,111 @@
+from dash import Dash, dcc, html, Input, Output
+from dash import dash_table as dt
+from src.helpers import load_data, prepare_meta, prepare_embeddings, \
+    update_index, find_closest_riders, make_rider_table, make_output_table
+
+app = Dash(__name__)
+
+############################## load & prepare data
+df = load_data()
+df_meta = prepare_meta(df)
+arr_emb, index, features = prepare_embeddings(df)
+
+RIDERS = df_meta["name"].tolist()
+COUNTRIES = df_meta["country"].unique().tolist()
+##################################################
+
+
+app.layout = html.Div([
+    ### HEADER ###
+    html.Div([
+
+        html.Div([
+            html.H2("Find similar pro cyclists"),
+            html.P("This is a tiny tool to find similar cyclists given a chosen rider. "
+                   "You can limit the pool based on age and country of origin. "
+                   "The data come from a community-created database for the game Pro Cycling Manager. "
+                   ),
+            html.P("Find more info on GitHub here. Have fun!")
+        ], style={"width": "45%"})
+
+    ]),
+
+    ### INPUT ###
+    html.Div([
+
+        html.Div([
+            html.H3("Pick a cyclist"),
+            dcc.Dropdown(
+                RIDERS,
+                "Jasper Philipsen",  # default value
+                id="rider"
+            )
+        ], style={"width": "30%", "margin-bottom": "5px"}),
+
+        html.Div(id="main-rider",
+                 style={"margin-top": "15px", "width": "80%"}),
+
+        html.Div([
+            html.Div([
+                html.H4("Country"),
+                dcc.Dropdown(
+                    COUNTRIES,
+                    "",  # default value
+                    multi=True,
+                    id="countries",
+                )], style={"width": "25%", "padding-right": "20px", "float": "left", "display": "inline-block"}),
+            html.Div([
+                html.H4("# of similar cyclists"),
+                dcc.Input(
+                    id="n_cyclists",
+                    type="number",
+                    value=5
+                )], style={"width": "12%", "display": "inline-block", "vertical-align": "top"}),
+            html.Div([
+                html.H4("Maximum age"),
+                dcc.Input(
+                    id="max_age",
+                    type="number",
+                    value=29
+                )], style={"width": "12%", "display": "inline-block", "vertical-align": "top"})
+        ], style={"padding-bottom": "15px"})
+
+    ]),
+
+    ### OUTPUT ###
+    html.Div([
+
+        html.Div(id="similar-riders",
+                 style={"margin-top": "15px", "padding-bottom": "10px", "width": "80%"})
+
+    ])
+])
+
+
+@app.callback(
+    Output("main-rider", "children"),
+    Output("similar-riders", "children"),
+    Input("rider", "value"),
+    Input("n_cyclists", "value"),
+    Input("max_age", "value"),
+    Input("countries", "value")
+)
+def update_similar_riders(rider, k=10, age_max=30, countries=""):
+    pop_indices = update_index(index, arr_emb, df_meta, age_max=age_max, countries=countries)
+    rider_i, D, I = find_closest_riders(index, arr_emb, rider=rider, riders_all=RIDERS, k=k)
+
+    # make main-rider output
+    out_rider = make_rider_table(rider_i, arr_emb, df_meta, features)
+    data_rider = out_rider.to_dict("rows")
+
+    # make similar-riders output
+    out_simil = make_output_table(I, arr_emb, df_meta, features, pop_indices)
+    data_simil = out_simil.to_dict("rows")
+
+    columns = [{"name": j, "id": j,} for j in (out_rider.columns)]
+
+    return dt.DataTable(data=data_rider, columns=columns), dt.DataTable(data=data_simil, columns=columns)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ src/helpers.py
@@ -0,0 +1,81 @@
+import faiss
+import pandas as pd
+import numpy as np
+
+def load_data(path="data/cyclists.xlsx"):
+    return pd.read_excel(path, engine="openpyxl")
+
+def prepare_meta(df):
+    df_meta = df.iloc[:, :6].copy()
+
+    # add full name
+    df_meta["name"] = df_meta["gene_sz_firstname"] + " " + df_meta["gene_sz_lastname"]
+
+    # add age
+    age = (pd.to_datetime("today") -
+           pd.to_datetime(df_meta["gene_i_birthdate"], format="%Y%m%d")).dt.days / 365
+    df_meta["age"] = np.floor(age.astype(int))
+
+    return df_meta
+
+def prepare_embeddings(df):
+    # cast to array structure
+    df_emb = df.iloc[:, 6:]
+    arr_emb = df_emb.values.astype(np.float32)  # should be an array
+    arr_emb = np.ascontiguousarray(arr_emb)  # should be c-contiguous
+
+    # prepare FAISS index
+    index = faiss.IndexFlatL2(arr_emb.shape[1])
+
+    # get cleaned characteristics columns
+    features = [f.replace("charac_i_", "") for f in df_emb.columns]
+
+    return arr_emb, index, features
+
+def update_index(index, arr_emb, df_meta, age_max, countries):
+    # apply filter(s)
+    if len(countries) != 0:
+        pop_indices = df_meta[(df_meta.age <= age_max) &
+                              (df_meta.country.isin(countries))].index
+    else:
+        pop_indices = df_meta[df_meta.age <= age_max].index
+
+    arr_emb_pop = arr_emb[pop_indices]
+
+    # update index
+    index.reset()
+    index.add(arr_emb_pop)
+
+    return pop_indices
+
+def find_closest_riders(index, arr_emb, rider, riders_all, k=5):
+    # get rider info to compare against
+    rider_i = riders_all.index(rider)  # list index, has nothing to do with the FAISS library
+    rider_i_emb = arr_emb[[rider_i]]
+
+    # get distances (D) and indices (I)
+    D, I = index.search(rider_i_emb, k + 1)  # search the k closest riders (drop first, will be rider_i)
+
+    return rider_i, D, I
+
+def make_rider_table(idx, arr_emb, df_meta, features):
+    # get cycling stats
+    stats = pd.DataFrame(arr_emb[idx], index=features).T
+
+    # construct DataFrame
+    df = pd.concat([df_meta.iloc[[idx]][["name", "age", "country"]].reset_index(drop=True), stats], axis=1)
+
+    return df
+
+def make_output_table(idxs, arr_emb, df_meta, features, pop_indices):
+    # subset first to filtered population
+    df_meta = df_meta.iloc[pop_indices]
+    arr_emb = arr_emb[pop_indices]
+
+    # get cycling stats
+    stats = pd.DataFrame(arr_emb[idxs[0][1:]], columns=features)
+
+    # construct DataFrame
+    df = pd.concat([df_meta.iloc[idxs[0][1:]][["name", "age", "country"]].reset_index(drop=True), stats], axis=1)
+
+    return df
